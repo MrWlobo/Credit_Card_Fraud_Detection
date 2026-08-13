3d1a2eaa6b31604b029c5548528c47123c4af164
MrWlobo <michaldworniczak40@gmail.com>
2026-08-13 20:22:46 +0200

    Created outline of training pipeline

---
+++ pipelines/training_pipeline.py
@@ -0,0 +1,12 @@
+from zenml import pipeline
+from steps.ingest_data import ingest_data
+from steps.clean_data import clean_data
+from steps.train_model import train_model
+from steps.evaluate_model import evaluate_model
+
+@pipeline
+def training_pipeline(data_path: str):
+    df = ingest_data(data_path)
+    X_train, y_train, X_test, y_test = clean_data(df)
+    train_model(X_train, y_train)
+    evaluate_model(X_test, y_test)
