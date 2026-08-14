b756d26910b74c2440332512d5c462e8d13a65b4
MrWlobo <michaldworniczak40@gmail.com>
2026-08-14 23:05:21 +0200

    Added logging to clean data step and sub-steps

--- src/steps/clean_data.py
+++ src/steps/clean_data.py
@@ -13,28 +13,44 @@ class DataCleaningStrategy(ABC):
 class DataHandleTypes(DataCleaningStrategy):
     @staticmethod
     def handle_data(df: pd.DataFrame) -> pd.DataFrame:
-        float_cols = [c for c in df.columns if c != "Class"]
-        
-        if "Class" in df.columns:
-            df["Class"] = df["Class"].astype("int32")
-            
-        if float_cols:
-            df[float_cols] = df[float_cols].astype("float64")
-            
-        return df
+        try:
+            float_cols = [c for c in df.columns if c != "Class"]
+
+            logging.info("Correcting data types")
+            if "Class" in df.columns:
+                df["Class"] = df["Class"].astype("int32")
+                
+            if float_cols:
+                df[float_cols] = df[float_cols].astype("float64")
+                
+            return df
+        except Exception as e:
+            logging.error(f"Error while correcting data types: {e}")
+            raise e
 
 class DataHandleNullValues(DataCleaningStrategy):
     @staticmethod
     def handle_data(df: pd.DataFrame) -> pd.DataFrame:
-        return df.fillna(df.mean(numeric_only=True))
+        try:
+            logging.info("Handling null values")
+            return df.fillna(df.mean(numeric_only=True))
+        except Exception as e:
+            logging.error(f"Error while handling null values: {e}")
+            raise e
 
 
 @step
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
-    data_cleaning_sub_steps = [
-        DataHandleTypes,
-        DataHandleNullValues,
-    ]
-    for sub_step in data_cleaning_sub_steps:
-        df = sub_step.handle_data(df)
-    return df
+    try:
+        logging.info("Cleaning data")
+        data_cleaning_sub_steps = [
+            DataHandleTypes,
+            DataHandleNullValues,
+        ]
+        for sub_step in data_cleaning_sub_steps:
+            df = sub_step.handle_data(df)
+
+        return df
+    except Exception as e:
+        logging.error(f"Error while cleaning data: {e}")
+        raise e
