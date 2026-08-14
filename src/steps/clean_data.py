3df5b0ee418d16e7be3c19eea3fa5f417a7653f5
MrWlobo <michaldworniczak40@gmail.com>
2026-08-14 19:07:29 +0200

    Added method for handling data types

--- src/steps/clean_data.py
+++ src/steps/clean_data.py
@@ -1,7 +1,26 @@
 import logging
 import pandas as pd
 from zenml import step
+from abc import ABC, abstractmethod
+
+
+class DataCleaningStrategy(ABC):
+    @abstractmethod
+    def handle_data(df: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+class DataHandleTypes(DataCleaningStrategy):
+    def handle_data(df):
+        for column in df.columns:
+            if column == "Class":
+                df[column] = df[column].astype("int32")
+                continue
+
+            df[column] = df[column].astype("float64")
+        return df
+
 
 @step
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    df = DataHandleTypes.handle_data(df)
     return df
