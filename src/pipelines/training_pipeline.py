004a8e407c84301a4400b230e04c76f201eb4f24
MrWlobo <michaldworniczak40@gmail.com>
2026-08-14 23:06:36 +0200

    Added data split to the pipeline

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -1,6 +1,7 @@
 from zenml import pipeline
 from steps.ingest_data import ingest_data
 from steps.clean_data import clean_data
+from steps.split_data import split_data
 from steps.train_model import train_model
 from steps.evaluate_model import evaluate_model
 
@@ -8,5 +9,6 @@ from steps.evaluate_model import evaluate_model
 def training_pipeline(data_path: str):
     df = ingest_data(data_path)
     cleaned_df = clean_data(df)
+    X_train, X_val, X_test, y_train, y_val, y_test = split_data(cleaned_df)
     train_model()
     evaluate_model()
