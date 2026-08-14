f3ce52750c6dee7075566afe364cefe5831a9254
MrWlobo <michaldworniczak40@gmail.com>
2026-08-14 23:00:27 +0200

    Added stratify to ensure equal ratios of fraudulent transactions across splits, added logging and annotations.

--- src/steps/split_data.py
+++ src/steps/split_data.py
@@ -1,30 +1,42 @@
 import logging
+from typing import Annotated, Tuple
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from zenml import step
-from typing import Tuple
 
 
 @step
 def split_data(
     df: pd.DataFrame,
 ) -> Tuple[
-    pd.DataFrame,
-    pd.DataFrame,
-    pd.DataFrame,
-    pd.Series,
-    pd.Series,
-    pd.Series,
+    Annotated[pd.DataFrame, "X_train"],
+    Annotated[pd.DataFrame, "X_val"],
+    Annotated[pd.DataFrame, "X_test"],
+    Annotated[pd.Series, "y_train"],
+    Annotated[pd.Series, "y_val"],
+    Annotated[pd.Series, "y_test"],
 ]:
-    X = df.drop(columns=["Class"])
-    y = df["Class"]
+    try:
+        X = df.drop(columns=["Class"])
+        y = df["Class"]
 
-    X_train_val, X_test, y_train_val, y_test = train_test_split(
-        X, y, test_size=0.15, random_state=42
-    )
+        logging.info("Splitting the dataset into training and testing sets")
+        X_train_val, X_test, y_train_val, y_test = train_test_split(
+            X, y, test_size=0.15, random_state=42, stratify=y
+        )
 
-    X_train, X_val, y_train, y_val = train_test_split(
-        X_train_val, y_train_val, test_size=0.1765, random_state=42
-    )
+        logging.info("Getting the validation set from training set")
+        X_train, X_val, y_train, y_val = train_test_split(
+            X_train_val,
+            y_train_val,
+            test_size=0.1765,
+            random_state=42,
+            stratify=y_train_val,
+        )
 
-    return X_train, X_val, X_test, y_train, y_val, y_test
+        return X_train, X_val, X_test, y_train, y_val, y_test
+
+    except Exception as e:
+        logging.error(f"Error while splitting the data: {e}")
+        raise e
+    
