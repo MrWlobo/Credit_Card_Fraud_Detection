edac60407991700ccbef48941e3c65409e96a108
MrWlobo <michaldworniczak40@gmail.com>
2026-08-15 08:55:29 +0200

    Abstract class for handling model training

--- src/steps/train_model.py
+++ src/steps/train_model.py
@@ -1,7 +1,26 @@
 import logging
 import pandas as pd
 from zenml import step
+from abc import ABC, abstractmethod
+
+
+class CreditCardFraudModel(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+    
+    @abstractmethod
+    def perform_model_training():
+        pass
+
 
 @step
-def train_model() -> None:
+def train_model(
+    X_train: pd.DataFrame,
+    X_val: pd.DataFrame,
+    X_test: pd.DataFrame,
+    y_train: pd.DataFrame,
+     y_val: pd.DataFrame,
+    y_test: pd.DataFrame,
+) -> None:
     pass
