fc7ea7896a27f76233463be20672679527cbdeb0
MrWlobo <michaldworniczak40@gmail.com>
2026-08-15 18:11:52 +0200

    Added MLFlow to the project

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
 from pipelines.training_pipeline import training_pipeline
 
 if __name__ == "__main__":
-    training_pipeline(data_path="data/creditcard.csv")
+    training_pipeline.with_options(enable_cache=False)(data_path="data/creditcard.csv")
--- src/steps/model_handling.py
+++ src/steps/model_handling.py
@@ -2,6 +2,8 @@ import logging
 from typing import Annotated, Tuple
 from sklearn.metrics import precision_score, recall_score, average_precision_score
 from zenml import step
+from zenml.client import Client
+import mlflow
 import numpy as np
 import torch
 import torch.nn as nn
@@ -9,7 +11,7 @@ from torch.utils.data import Dataset, DataLoader
 
 
 class CreditCardFraudModel(nn.Module):
-    def __init__(self, input_size: int):
+    def __init__(self):
         super().__init__()
         self.loss_fn = nn.BCEWithLogitsLoss()
 
@@ -83,7 +85,10 @@ class BasicCreditCardFraudModel(CreditCardFraudModel):
     def forward(self, x) -> torch.Tensor:
         return self.model_stack(x)
 
-@step
+
+experiment_tracker = Client().active_stack.experiment_tracker
+
+@step(experiment_tracker=experiment_tracker.name)
 def train_and_validate_model(
     train_dataset: Dataset,
     val_dataset: Dataset, 
@@ -98,6 +103,10 @@ def train_and_validate_model(
     model = BasicCreditCardFraudModel(input_size=feature_count)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
+    mlflow.log_param("epochs", epochs)
+    mlflow.log_param("batch_size", batch_size)
+    mlflow.log_param("feature_count", feature_count)
+
     logging.info("Starting model training")
     for epoch in range(epochs):
         train_loss = model.train_epoch(train_loader, optimizer)
@@ -106,9 +115,16 @@ def train_and_validate_model(
             f"Epoch {epoch+1}/{epochs} | Train Loss: {train_loss:.4f} | Val Loss: {val_loss:.4f} | Accuracy: {val_acc*100:.2f}% | Precision: {val_pre:.2f} | Recall: {val_rec:.2f} | AUPRC: {val_auprc:.2f}"
         )
 
+        mlflow.log_metric("train_loss", train_loss, step=epoch)
+        mlflow.log_metric("val_loss", val_loss, step=epoch)
+        mlflow.log_metric("val_accuracy", val_acc, step=epoch)
+        mlflow.log_metric("val_precision", val_pre, step=epoch)
+        mlflow.log_metric("val_recall", val_rec, step=epoch)
+        mlflow.log_metric("val_auprc", val_auprc, step=epoch)
+
     return model
 
-@step
+@step(experiment_tracker=experiment_tracker.name)
 def evaluate_model(
     test_dataset: Dataset,
     model: nn.Module,
@@ -122,4 +138,11 @@ def evaluate_model(
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
     test_loss, test_acc, test_pre, test_rec, test_auprc = model.evaluate(test_loader)
     logging.info(f"Final Test Evaluation | Loss: {test_loss:.4f} | Accuracy: {test_acc*100:.2f}% | Precision: {test_pre:.2f} | Recall: {test_rec:.2f} | AUPRC: {test_auprc:.2f}")
+    
+    mlflow.log_metric("test_loss", test_loss)
+    mlflow.log_metric("test_accuracy", test_acc)
+    mlflow.log_metric("test_precision", test_pre)
+    mlflow.log_metric("test_recall", test_rec)
+    mlflow.log_metric("test_auprc", test_auprc)
+    
     return test_acc, test_pre, test_rec, test_auprc
